8958413f621f52e55883bceab884a88beead9951
Kunal Dharpure <146354402+kunald08@users.noreply.github.com>
2025-01-29 00:12:08 +0530

    Initial commit

---
+++ dashboard/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from . models import *
+
+# Register your models/classes here.
+admin.site.register(Notes)
+admin.site.register(Homework)
+admin.site.register(Todo)
+# admin.site.register(Profile)
